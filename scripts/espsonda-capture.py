415cad31c131d41062f26fd7d03cf8dbb49b3500
dudeku <klaudiusz123dudek@gmail.com>
2025-01-31 14:56:39 +0100

    clean up measurement data

--- scripts/espsonda-capture.py
+++ scripts/espsonda-capture.py
@@ -3,8 +3,64 @@ import json
 import os
 import socket
 
-current_date = time.strftime('%d_%m_%Y')
-filename = f'espsonda_{current_date}.json'
+from tango import DeviceProxy, DevFailed, ConnectionFailed
+from typing import Tuple
+from collections import namedtuple
+
+import sys
+
+# Initialise tango proxy objects
+bim1 = DeviceProxy("tango.machine:10000/R1-ALL/DIA/R1-ALL-DIA-BIM1")
+
+# Named tuple, storing information about a device
+Device = namedtuple(
+    'Device', ['name', 'proxy', 'attribute', 'error_value']
+)
+
+devices = (
+    Device('R1-ALL-DIA-BIM1', bim1, 'BeamCurrent', -1.),
+)
+
+readings = []
+device_failure = []
+
+# Fetch data from tango
+for device in devices:
+	try:
+		reading = device.proxy.read_attribute(device.attribute).value
+		print("Device server value read")
+	except (DevFailed, ConnectionFailed):
+		reading = device.error_value
+		device_failure.append(device.name)
+		print("Error reading device server")
+
+print(reading * 1e3)
+
+time.sleep(1)
+
+# readings.append(reading )
+
+#capture_t = int(sys.argv[1])
+if (int(sys.argv[1]) != 0):
+    capture_t = int(sys.argv[1])
+else:
+    capture_t = 1
+
+if (int(sys.argv[2]) != 0):
+    capture_f = int(sys.argv[2])
+else:
+    capture_f = 1
+    
+print(f"t: {capture_t}")
+print(f"f: {capture_f}")
+
+
+output_dir = "measurements"
+os.makedirs(output_dir, exist_ok=True)
+
+current_date = time.strftime('%H-%M-%S_%d-%m-%Y')
+filename = os.path.join(output_dir, f'espsonda_{current_date}.json')
+#filename = f'espsonda_{current_date}.json'
 
 #filename = 'new_json_test.json'
 
@@ -15,7 +71,7 @@ if os.path.exists(filename) and os.path.getsize(filename) > 0 :
         json_data = json.load(file)
 else:
     # If the file doesn't exist, initialize the JSON structure
-    json_data = {'epochtime': [], 'board': []}
+    json_data = {'epochtime': [],'current': [], 'board': []}
 
 #connect to espsonda
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,15 +80,33 @@ print('connecting to %s port %s' % server_address)
 sock.connect(server_address)
 
 # Append new data multiple times
-for x in range(36000):
+for x in range(int(capture_t*capture_f)):
     # Append the current epoch time
     epochtime = int(time.time())
+    timing1 = time.time()
+    # print(timing1)
     json_data['epochtime'].append(epochtime)
+
+    for device in devices:
+        try:
+            reading = device.proxy.read_attribute(device.attribute).value
+            print("Device server value read")
+        except (DevFailed, ConnectionFailed):
+            reading = device.error_value
+            device_failure.append(device.name)
+            print("Error reading device server")
+
+    readings.append(reading)
+
+    json_data['current'].append(reading)
 
     message = '?'
     sock.sendall(message.encode(encoding='utf-8'))
 
-    time.sleep(0.1)
+    #time.sleep(0.05)
+    #time.sleep(0.2)
+    time.sleep(float(1/capture_f))
+
     data = sock.recv(512).decode('utf-8')
     
     try:
@@ -44,7 +118,9 @@ for x in range(36000):
             print("Invalid data format: 'board' key not found.")
     except json.JSONDecodeError:
         print("Error decoding JSON from received data.")
-    print("New data received.")
+    print(time.time() - timing1)
+    print(f"[{x+1}/{capture_t*capture_f}]: New data received.")
+
 
 # Write the updated JSON data back to the file
 with open(filename, 'w') as file:
